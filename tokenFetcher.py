daf05fb5bd9cfc5844791357290c2a80d498a89e
Jon <53925371+jondidathing@users.noreply.github.com>
2023-02-23 09:19:16 -0500

    update

--- tokenFetcher.py
+++ tokenFetcher.py
@@ -1,4 +1,5 @@
 # imports
+from getPool import extractAddress
 import os
 from web3 import Web3, HTTPProvider
 from dotenv import load_dotenv, find_dotenv
@@ -12,44 +13,23 @@ from polygonscan import PolygonScan
 # LOADING the .env file
 load_dotenv(find_dotenv())
 
-# API rate limiter to ensure I don't go over my calls per second
-rate_limiter = RateLimiter(max_calls=3, period=2)
+
 # the parameter is the unique tx hash of the tx being queries & the specific blockexplorer the tx is coming from
 def getTokens(txHash:str, blockExplorer:str):
     # checks the blockexploer given arguement from TA and determines which blockchain to connect to
     match blockExplorer:
         case 'polygonscan.com/tx/':
             # instantiate a web3 remote provider
-            endPoint = 'https://matic.getblock.io/6740c819-7a27-4382-93c2-d029ac4b0a97/mainnet/'
-            try:
-                w3 = Web3(HTTPProvider(endPoint))
-            except Exception as err:
-                print(err)
-        case 'etherscan.io/tx/':
-            # instantiate a web3 remote provider
-            endPoint = 'https://matic.getblock.io/6740c819-7a27-4382-93c2-d029ac4b0a97/mainnet/'
-            try:
-                w3 = Web3(HTTPProvider(endPoint))
-            except Exception as err:
-                print(err)
-        case 'bscscan.com/tx/':
-            # instantiate a web3 remote provider
-            endPoint = 'https://bsc.getblock.io/6740c819-7a27-4382-93c2-d029ac4b0a97/mainnet/'
-            try:
-                w3 = Web3(HTTPProvider(endPoint))
-            except Exception as err:
-                print(err)
-        case 'snowtrace.io/tx/':
-            # instantiate a web3 remote provider
-            endPoint = 'https://avax.getblock.io/mainnet/6740c819-7a27-4382-93c2-d029ac4b0a97/ext/bc/C/rpc'
+            endPoint = os.getenv('endpoint')
             try:
                 w3 = Web3(HTTPProvider(endPoint))
             except Exception as err:
                 print(err)
 
+
     # the tx receipt for to gather all tokens which were traded
-    txDict = w3.eth.get_transaction_receipt(txHash)
-    tokenAdressList = [str(eachElement.address) for eachElement in txDict.logs]
+    txReceipt = w3.eth.get_transaction_receipt(txHash)
+    tokenAdressList = [str(eachElement.address) for eachElement in txReceipt.logs]
     # this takes each element inside of the list and map it a dictionary deleting any   duplicates and assign NO values inside of the dictionary - from stackoverflow
     uniqueTokenAddressList = list(dict.fromkeys(tokenAdressList))
     # dict for storing token details
@@ -75,6 +55,10 @@ def getTokens(txHash:str, blockExplorer:str):
 
         # the contract of the given address
         tokenContract = w3.eth.contract(address=eachAddress, abi=abi)
+        
+        # API rate limiter to ensure I don't go over my calls per second
+        rate_limiter = RateLimiter(max_calls=3, period=2)
+        
         with rate_limiter:
             # contrainsts to look for ONLYERC20 Tokens based on what is usually found in them 
             erc20TokenFunctionList = ['<Function balanceOf(address)>', '<Functionname()>', '<Function totalSupply()>', '<Function symbol()>', '<Functiondecimals()>', '<Function transfer(address,uint256)>']
@@ -88,6 +72,10 @@ def getTokens(txHash:str, blockExplorer:str):
                 tokenDecimals = tokenContract.functions.decimals().call()
                 cg = CoinGeckoAPI()
                 if cg.search(tokenSymbol)['coins'] != []:
-                    tokenDict.update({f"{tokenSymbol}":[tokenName,tokenDecimals,eachAddress]})
+                    #tokenDict.update({f"{tokenSymbol}":{tokenName,tokenDecimals,eachAddress}})
+                    tokenDict.update(
+                        {tokenSymbol: {'name':tokenName, 'address': eachAddress, 'decimals': tokenDecimals}}
+                    )
+
     print(tokenDict)
-    return tokenDict
+    extractAddress(tokenDict)
